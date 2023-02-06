d6df7e51eacdc1ed8fb2e7e63c8fb959fbb5690b
Philip Preisler <phil313c@edu.zealand.dk>
2023-02-06 13:35:06 +0100

    Add project files.

---
+++ SharedCalculator/BasicMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculator
+{
+    public static class BasicMath 
+    {
+        //Takes two doubles as parameters and ADDS them together where sum is the result being return
+        public static double Addition(double x, double y)
+        {
+            double sum = 0;
+            sum = x + y;
+            return sum;
+        }
+        //Takes two doubles as parameters and SUBTRACTS them where sum is the result being return
+        public static double Subtraction(double x, double y)
+        {
+            double sum = 0;
+            sum = x - y;
+            return sum;
+        }
+        //Takes two doubles as parameters and MULTIPLIES them where sum is the result being return
+        public static double Multiplication(double x, double y)
+        {
+            double sum = 0;
+            sum = x * y;
+            return sum;
+        }
+        //Takes two doubles as parameters and DIVIDES them where sum is the result being return
+        public static double Division(double x, double y)
+        {
+            double sum = 0;
+            sum = x / y;
+            return sum;
+        }
+    }
+}
---
+++ SharedCalculatorTests/BasicMathTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculator.Tests
+{
+    [TestClass()]
+    public class BasicMathTests
+    {
+        //If test passes then Addition method should be working
+        [TestMethod()]
+        public void AdditionTest()
+        {
+            double testingAdditionTrue = 10.15;
+            double v = BasicMath.Addition(3.15, 7);
+            Assert.AreEqual(testingAdditionTrue, v);
+        }
+        //If test passes then Subtraction method should be working
+        [TestMethod()]
+        public void SubtractionTest()
+        {
+            double testingSubtractionTrue = 4.2;
+            double v = BasicMath.Subtraction(37, 32.8);
+            Assert.AreEqual(testingSubtractionTrue, v,0.0005);
+        }
+        //If test passes then Multiplication method should be working
+        [TestMethod()]
+        public void MultiplicationTest()
+        {
+            double testingMultiplicationTrue = 44;
+            double v = BasicMath.Multiplication(10, 4.4);
+            Assert.AreEqual(testingMultiplicationTrue, v);
+        }
+        //If test passes then disvion method should be working
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            double testingDivisionTrue = 15;
+            double v = BasicMath.Division(240, 16);
+            Assert.AreEqual(testingDivisionTrue, v);
+        }
+    }
+}
